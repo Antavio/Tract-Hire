bafb545daaf44f3f97cf79c292f60c4cb1a3eecb
Antavio <njuguna13@gmail.com>
2019-06-05 14:03:53 +0300

    Create a fetch_all_tractors method to fetch all attributes of a Tractor.

--- tract_hire/models.py
+++ tract_hire/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tractor(models.Model):
+    name = models.CharField(max_length=255)
+    tractor_image = models.ImageField(upload_to='tractor_pics',blank=True)
+    category = models.CharField(max_length=200)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    price_estimate = models.FloatField()
+    location = models.CharField(max_length=255)
+    contact = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-id']
+
+    def save_tractor(self):
+        self.save()
+
+    def delete_tractor(self):
+        self.delete()
+
+    @classmethod
+    def fetch_all_tractors(cls):
+        all_tractors = Tractor.objects.all()
+        return all_tractors
+
+    @classmethod
+    def search_tractor(cls,search_term):
+        tractor = cls.objects.filter(category__icontains=search_term)
+        return tractor   
