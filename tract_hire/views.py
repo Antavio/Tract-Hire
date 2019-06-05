24bfec7ce5bc1c0667d34c9863ba1f08cfb16719
Antavio <njuguna13@gmail.com>
2019-06-05 20:39:19 +0300

    Create a view function filter_by_location.

--- tract_hire/views.py
+++ tract_hire/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     tractors = Tractor.fetch_all_tractors()
-    return render(request,'tract_hire/index.html',{"tractors":tractors})
+    location = Location.get_location()
+    return render(request,'tract_hire/index.html',{"tractors":tractors,"locations":location})
 
 def search_category(request):
     if 'category' in request.GET and request.GET ["category"]:
@@ -26,3 +27,11 @@ def tractor_details(request,tractor_id):
     except Exception as  e:
         raise Http404()
     return render(request,'tract_hire/tractor_details.html',{"single_tractor":single_tractor})
+
+def filter_by_location(request,tractor_id):
+    try:
+        location = Location.get_location()
+        located_tractors = Tractor.objects.filter(location_id=tractor_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,'tractor_location/filter_location.html',{"located_tractors":located_tractors,"locations":location})
