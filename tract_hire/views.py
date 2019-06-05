5e3a4f5fcadcd2294b52f0236b41c8e18c87c904
Antavio <njuguna13@gmail.com>
2019-06-05 14:43:27 +0300

    Create a view function to render search template.

--- tract_hire/views.py
+++ tract_hire/views.py
@@ -4,3 +4,15 @@ from .models import *
 def index(request):
     tractors = Tractor.fetch_all_tractors()
     return render(request,'tract_hire/index.html',{"tractors":tractors})
+
+def search_category(request):
+    if 'category' in request.GET and request.GET ["category"]:
+        search_term = request.GET.get("category")
+        searched_tractors = Tractor.search_tractor(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search/search.html', {"message":message, "searched_tractors":searched_tractors})
+
+    else:
+        message = "No search results yet!"
+        return render (request, 'search/search.html', {"message": message})
