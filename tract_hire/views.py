80aac73d1d5d0ae9d9f8935c1d41d7ff3f904294
Antavio <njuguna13@gmail.com>
2019-06-06 19:49:17 +0300

    Create profile,new_profile and edit view functions.

--- tract_hire/views.py
+++ tract_hire/views.py
@@ -4,6 +4,7 @@ from .forms import *
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .email import send_welcome_email
+from django.core.exceptions import ObjectDoesNotExist
 import datetime
 # Create your views here.
 def index(request):
@@ -62,3 +63,43 @@ def new_tractor(request):
     else:
         form = TractorForm()
     return render(request,"upload_tractor/new_tractor.html",{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.prof_user = current_user
+            profile.profile_Id = request.user.id
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'profile/new_profile.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user = request.user
+    if request.method == 'POST':
+        logged_user = Profile.objects.get(prof_user=request.user)
+        form = ProfileForm(request.POST, request.FILES, instance=logged_user)
+        if form.is_valid():
+            form.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request,'profile/edit_profile.html',{'form':form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    tractors = Tractor.objects.filter(user = current_user)
+
+    try:   
+        prof = Profile.objects.get(prof_user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile/profile.html',{'profile':prof,'tractors':tractors})
